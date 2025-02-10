db6f577e93874142f8fb45809fa976feab66da75
hong9640 <hong9640@naver.com>
2025-02-10 17:55:48 +0900

    backjhon

--- pracitce.py
+++ pracitce.py
@@ -1,3 +1,6 @@
-king, queen, rock, bishop, knight, phon = map(int, input().split())
-k = "1", q = "1", r = "2", b = "2", k = "2", p = "8"
-print(king - k, queen - q, rock - r, bishop - b, knight - k, phon - p)
+chess = map(int, input().split())
+k = 1, q = 1, r = 2, b = 2, k = 2, p = 8
+for i in range(len(chess)):
+    if chess[i] > 0:
+
+
