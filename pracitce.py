417094a963c2d621a8230b37622b040173ff31e2
hong9640 <hong9640@naver.com>
2025-02-10 17:44:19 +0900

    prac

---
+++ pracitce.py
@@ -0,0 +1,3 @@
+king, queen, rock, bishop, knight, phon = map(int, input().split())
+k = "1", q = "1", r = "2", b = "2", k = "2", p = "8"
+print(king - k, queen - q, rock - r, bishop - b, knight - k, phon - p)
